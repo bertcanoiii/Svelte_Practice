a7501fa9d970ec516ba67ba9d7fbef182bfd1c13
Bert Cano <bertcanoiii@gmail.com>
2022-07-29 09:03:08 -0700

    figured out how to change background color, removed boilerplate/starting css

--- Practice_0727/Practice_0727/src/App.svelte
+++ Practice_0727/Practice_0727/src/App.svelte
@@ -6,9 +6,15 @@
   }
 </script>
 
-<main>
-  <div class="flex flex-col justify-center items-center md:flex-row max-w-6xl bg-slate-500 p-20 rounded-3xl">
+<main class="bg-blue-50 h-screen w-screen">
+  <div class="flex flex-col justify-center md:flex-row max-w-3xl bg-slate-500 p-20 rounded-3xl">
     <Login on:message={handleMessage}/>
+  </div>
+
+  <div class="flex flex-col md:flex-row justify-center bg-slate-500 p-20 rounded-3xl">
+    <textarea class="text-black">
+
+    </textarea>
   </div>
 </main>
 
